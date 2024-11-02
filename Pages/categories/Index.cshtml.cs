3af2c3d6e530cae58c959c896035bc2ebcf717fb
kgomez <kgomez@DESKTOP-SQRSML0>
2024-11-02 09:32:31 -0500

    Index Categories

---
+++ Pages/categories/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB_origins.Data;
+using SupermarketWEB_origins.Models;
+
+namespace SupermarketWEB_origins.Pages.categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public IndexModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Categories { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Categories != null)
+            {
+                Categories = await _context.Categories.ToListAsync();
+            }
+        }
+    }
+}
