e2c8600d6d3703bff636945342065d1a3e23a3f5
kgomez <kgomez@DESKTOP-SQRSML0>
2024-11-05 13:24:39 -0500

    create Category

---
+++ Pages/product/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB_origins.Data;
+using SupermarketWEB_origins.Models;
+
+namespace SupermarketWEB_origins.Pages.product
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Products == null || Product == null)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
